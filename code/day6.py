86b3c3356e000f349f37f10e8f093b5eee5b6d62
Simon Edvardsson <Simon.Edvards@gmail.com>
2018-12-06 10:51:21 +0100

    Cleaned up part A so you get the answer printed

--- code/day6.py
+++ code/day6.py
@@ -1,13 +1,12 @@
 from collections import Counter
-
 '''
 Todays part a is now superslow because i loop through -500 - 1000
 to be able to filter out the infinite ones by eye. 
 Look through the print and look for the first element that havent changed much.
 '''
-def create_map(dic) :
+def create_map(dic, width, height) :
     world_map = dict()
-    a = [ (i, j) for i in range(-20,50) for j in range(-20, 50)]
+    a = [ (i, j) for i in range(width,height) for j in range(width, height)]
     for i in a :
         temp = 100000
         for key, value in dic.items() :
@@ -18,7 +17,7 @@ def create_map(dic) :
                 world_map[i] = key
                 temp = dist
     new_dic = Counter(world_map.values())
-    print(new_dic)
+    return new_dic
 
 
 ''' Part b method to solve the size of area '''
@@ -26,26 +25,34 @@ def count_area(dic) :
     a = [(i, j) for i in range(0,350) for j in range(0, 350)]
     count = 0
     for i in a :
-        limit = 10000
-        length = 0
+        limit, length = 10000, 0
         for value in dic.values() :
             length += count_distance(i, value)
         if length < limit :
             count += 1
     return count
 
+''' Compare two diffrent dicts and takes the biggest value that is the same 
+    for both dicts. That value must be the largest not infinite number'''
+def find_biggest_area(dic) :
+    distances = []
+    dic_one = create_map(dic, 0, 350)
+    dic_two = create_map(dic, -1, 351)
+    for key, value in dic_one.items() :
+        if value == dic_two[key] :
+            distances.append(value)
+    return (max(distances))
 
-
-
+''' Counts the manhattan distance '''
 def count_distance(t1, t2) :
     dist = abs(t1[0] - t2[0]) + abs(t1[1] - t2[1])
     return dist
 
 
-dic = dict()
 with open("../inputs/day6.txt", "r") as f :
+    dic = dict()
     for i, line in enumerate(f) :
         x, y = line.replace("\n", "").split(",")
         dic[i] = (int(x),int(y))
-    create_map(dic)
-    print(count_area(dic))
+    print("Largest area is {} and the location area for part b is {}"
+          .format(find_biggest_area(dic), count_area(dic)))
